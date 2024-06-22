92b8f452f36a390c4e28bbec0538946fb54b2c4d
Sebastian Grzybowski <Seba9989@localhost.localdomain>
2024-06-22 14:24:34 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<img src="My-New-Logo-Alt.webp" alt="Logo" class=" max-h-[30vh]" />
+<div class="flex flex-col gap-4">
+	<h1 class="font-bold text-4xl text-pretty text-center">
+		Hello I am Seba9989, my portfolio is under renovation.
+	</h1>
+	<h2
+		class="font-bold text-6xl bg-clip-text text-transparent bg-gradient-to-r from-fuchsia-700 to-cyan-500 text-center"
+	>
+		We will be back soon.
+	</h2>
+</div>
